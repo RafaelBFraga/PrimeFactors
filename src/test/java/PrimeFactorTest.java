25f1a8e7a29c0dd25f9dfee14a306f4d8d767385
Rafael Fraga <rfraga@thoughtworks.com>
2016-02-22 14:24:11 -0300

    Added test to generate

--- src/test/java/PrimeFactorTest.java
+++ src/test/java/PrimeFactorTest.java
@@ -1,5 +1,9 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 /**
@@ -9,11 +13,12 @@ public class PrimeFactorTest {
 
     @Test
     public void shouldGeneratePrimeFactor() {
-
+        PrimeFactor primeFactor = new PrimeFactor();
+        List<Integer> primeFactors = new ArrayList<>();
+        primeFactors.add(2);
+        primeFactors.add(3);
+        primeFactors.add(5);
+        assertThat(primeFactor.generate(30),is(primeFactors));
     }
 
-    @Test
-    public void shouldPrintList() {
-
-    }
 }
